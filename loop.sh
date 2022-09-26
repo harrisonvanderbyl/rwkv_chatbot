2e658c77e672883d7dc475589916b06f9bc75c0e
harrison <harrison.vanderbyl@gmail.com>
2022-09-26 20:55:57 +1000

    loop

--- loop.sh
+++ loop.sh
@@ -1,6 +1,9 @@
 #run chatbot, if it crashes, pull from git and run again
 #this is a loop, so it will keep running until you kill it
 
+#load environment variables from .env
+export $(cat .env | xargs)
+
 while true
 do
     python3 chat.py
