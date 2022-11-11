d8d29ea9b8c39c8113b675bd6b77f29b460909f9
harrison <harrison.vanderbyl@gmail.com>
2022-11-11 20:00:53 +1100

    fix?

--- src/model_run.py
+++ src/model_run.py
@@ -207,7 +207,7 @@ class RWKV_RNN(nn.Module):
                 x = x + pos_emb
 
             if self.RUN_DEVICE == 'cuda':
-                x = x.to(device=self.procDevice, non_blocking=True)
+                x = x.to(device="cuda:0", non_blocking=True)
 
             for o in range(self.n_layer):
                 i = o
