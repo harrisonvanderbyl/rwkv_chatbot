accfd9453d7028c8684fecc86ee9a56b90ef1120
harrison <harrison.vanderbyl@gmail.com>
2022-12-22 06:30:46 +1100

    fix

--- chat.py
+++ chat.py
@@ -1,83 +1,94 @@
-########################################################################################################
-# The RWKV Language Model - https://github.com/BlinkDL/RWKV-LM
-########################################################################################################
-
-import loadModelForOnnx
 import discord
+import matplotlib.pyplot as plt
+import loadModelForOnnx
+import tqdm
+import onnxruntime as ort
 from genericpath import exists
 from typing import List
-from torch.nn import functional as F
-from src.model_run import RWKV_RNN
 import numpy as np
-import math
 import os
-import sys
-import types
 import time
 import gc
 import torch
 from src.utils import TOKENIZER
-from tqdm import tqdm
-try:
-    os.environ["CUDA_VISIBLE_DEVICES"] = sys.argv[1]
-except:
-    pass
+from src.rwkvops import RwkvOpList
+import inquirer
+from scipy.special import softmax
+from torch.nn import functional as F
+from torch.profiler import profile, record_function, ProfilerActivity
+import src.model_run_onnx as mro
+from sty import Style, RgbFg, fg
 
-########################################################################################################
-# Step 2: set prompt & sampling stuffs
-########################################################################################################
+fg.orange = Style(RgbFg(255, 150, 50))
 
 # context = 'A'
 # context = "\nIn the"
 # context = '\nSugar:'
-
+# context = "\n深圳是" # test Chinese
+# context = "\n東京は" # test Japanese
 # context = "\n深圳是" # test Chinese
 # context = "\n東京は" # test Japanese
 
+files = os.listdir()
+# filter by ending in .pth
+files = [f for f in files if f.endswith(".pth")]
+
+questions = [
+    inquirer.List('file',
+                  message="What model do you want to use?",
+                  choices=files,
+                  ),
+    inquirer.List(
+        'method',
+        message="What inference method?",
+        choices=RwkvOpList.keys()
+    )
+]
+q = inquirer.prompt(questions)
+
+model, emptyState = mro.createRWKVModel(
+    q["file"], mode=q["method"])
+
 ###### A good prompt for chatbot ######
-user = "User"
-interface = ":"
-bot = "RWKV"
-context = f'''
-The following is a conversation between a highly knowledgeable and intelligent AI assistant called {bot}, and a human user called {user}. In the following interactions, {user} and {bot} converse in natural language, and {bot} always answer {user}'s questions. {bot} is very smart, polite and humorous. {bot} knows a lot, and always tells the truth. The conversation begins.
+context = '''
+The following is a conversation between a highly knowledgeable and intelligent AI assistant, called RWKV, and a human user, called User. In the following interactions, User and RWKV will converse in natural language, and RWKV will do its best to answer User’s questions. RWKV was built to be respectful, polite and inclusive. It knows a lot, and always tells the truth. The conversation begins.
 
-{user}{interface} who is president of usa?
+User: OK RWKV, I’m going to start by quizzing you with a few warm-up questions. Who is currently the president of the USA?
 
-{bot}{interface} It’s Joe Biden; he was sworn in earlier this year.
+RWKV: It’s Joe Biden; he was sworn in earlier this year.
 
-{user}{interface} french revolution what year
+User: What year was the French Revolution?
 
-{bot}{interface} It started in 1789, but it lasted 10 years until 1799.
+RWKV: It started in 1789, but it lasted 10 years until 1799.
 
-{user}{interface} guess i marry who ?
+User: Can you guess who I might want to marry?
 
-{bot}{interface} Only if you tell me more about yourself - what are your interests?
+RWKV: Only if you tell me more about yourself - what are your interests?
 
-{user}{interface} wat is lhc
+User: Aha, I’m going to refrain from that for now. Now for a science question. What can you tell me about the Large Hadron Collider (LHC)?
 
-{bot}{interface} It’s a large and very expensive piece of science equipment. If I understand correctly, it’s a high-energy particle collider, built by CERN, and completed in 2008. They used it to confirm the existence of the Higgs boson in 2012.
-
+RWKV: It’s a large and very expensive piece of science equipment. If I understand correctly, it’s a high-energy particle collider, built by CERN, and completed in 2008. They used it to confirm the existence of the Higgs boson in 2012.
 '''
 # context = "hello world! I am your supreme overlord!"
 NUM_TRIALS = 999
 LENGTH_PER_TRIAL = 200
 
-TEMPERATURE = 0.9
-top_p = 0.9
+TEMPERATURE = 1.0
+top_p = 0.8
 top_p_newline = 0.9  # only used in TOKEN_MODE = char
 
 DEBUG_DEBUG = False  # True False --> show softmax output
 
 ########################################################################################################
 
-model = loadModelForOnnx.loadModelCompat()
-
 
 print(f'\nOptimizing speed...')
+
 gc.collect()
 torch.cuda.empty_cache()
 
 # input(0)
+
 TOKEN_MODE = "pile"
 WORD_NAME = [
     "20B_tokenizer.json",
@@ -90,6 +101,10 @@ if TOKEN_MODE == "pile":
     assert tokenizer.tokenizer.decode([187]) == '\n'
 
 ########################################################################################################
+
+
+ctx1 = tokenizer.tokenizer.encode(context)
+src_ctx1 = ctx1.copy()
 
 
 print(
@@ -120,7 +135,24 @@ print("torch.cuda.max_memory_reserved: %fGB" %
       (torch.cuda.max_memory_reserved(0)/1024/1024/1024))
 
 
+def loadContext(ctx: list[int], newctx: list[int], statex):
+    # with profile(activities=[ProfilerActivity.CPU], record_shapes=True) as prof:
+    #     with record_function("model_inference"):
+    with torch.jit.optimized_execution(True):
+        for i in tqdm.tqdm(range(len(newctx))):
+
+            x = ctx+newctx[:i+1]
+
+            o = model.forward([x[-1]], statex)
+            statex = o[1]
+            # with profile(activities=[ProfilerActivity.CPU], record_shapes=True) as prof:
+    #     with record_function("model_inference"):
+    # print(prof.key_averages().table(sort_by="cpu_time_total", row_limit=10))
+    return ctx+newctx, o[1]
+
+
 # bot.py
+
 
 client = discord.Client(
     intents=discord.Intents.all())
@@ -131,21 +163,8 @@ async def on_ready():
     print(f'{client.user} has connected to Discord!')
 
 
-def loadContext(self, ctx: list[int], newctx: list[int], statex):
-
-    for i in tqdm(range(len(newctx))):
-
-        x = ctx+newctx[:i+1]
-        o = [[x[-1]], statex]
-        ss = [pre]+self
-        for s in ss:
-            o = s.forward(*o)
-
-    return ctx+newctx, o[1]
-
-
 # init empty save state and question context
-init_state = model.empty_state()
+init_state = emptyState
 model_tokens = tokenizer.tokenizer.encode(context)
 
 
@@ -153,13 +172,32 @@ saveStates = {}
 saveStates["empty"] = ([187, 187], init_state.clone())
 
 # Put the prompt into the init_state
-init_state = model.loadContext([187, 187], model_tokens, init_state)
-saveStates["questions"] = (init_state[0], init_state[1].clone())
+init_state = loadContext([187, 187], model_tokens, init_state)
+saveStates["questions"] = (init_state[0], init_state[1])
 
 src_model_tokens = model_tokens.copy()
 currstate = init_state
 
 storys = []
+
+
+def sample_logits(ozut, temp: float = 1.0, top_p_usual: float = 0.8) -> int:
+
+    # out[self.UNKNOWN_CHAR] = -float('Inf')
+    # out[self.UNKNOWN_CHAR] = -float('Inf')
+    # turn to float if is half and cpu
+    probs = softmax(ozut, axis=-1)
+
+    sorted_probs = np.sort(probs)[::-1]
+    cumulative_probs = np.cumsum(sorted_probs)
+    cutoff = float(sorted_probs[np.argmax(cumulative_probs > top_p)])
+    probs[probs < cutoff] = 0
+    if temp != 1.0:
+        probs = probs.pow(1.0 / temp)
+    probs = probs / np.sum(probs)
+    out = np.random.choice(a=len(probs), p=probs)
+
+    return out
 
 
 @client.event
@@ -199,20 +237,27 @@ async def on_message(message):
     if msg[:6] == '+drkv ':
 
         real_msg = msg[6:].strip()
-        new = f"\n\{user}{interface} {real_msg}\n\n{bot}:"
+        new = f"\nUser: {real_msg}\n\nRWKV:"
         tknew = tokenizer.tokenizer.encode(new)
         print(f'### add ###\n[{new}]')
 
-        begin = len(currstate[0] + tknew)
-
-        currstate = model.loadContext(currstate[0], tknew, currstate[1])
+        currstate = loadContext(currstate[0], tknew, currstate[1])
+        begin = len(currstate[0])
         state = currstate
-        with torch.no_grad():
-            state = model.run(
-                currstate=[{"score": 0, "ctx": state[0], "state":state[1]}])
 
-        currstate = (state[0]["ctx"], state[0]["state"])
-        send_msg = tokenizer.tokenizer.decode(currstate[0][begin:]).strip()
+        with torch.no_grad():
+            for i in range(100):
+                o = model.forward([state[0][-1]], state[1])
+                tok = sample_logits(o[0])
+                print(tokenizer.tokenizer.decode(tok), end='')
+                state = (state[0] + [tok], o[1])
+                if (tok == 187 and i > 10):
+                    break
+
+        send_msg = tokenizer.tokenizer.decode(state[0][begin:]).strip()
+        currstate = state
+        if (len(send_msg) == 0):
+            send_msg = "Error: No response generated."
         print(f'### send ###\n[{send_msg}]')
         await message.reply(send_msg)
     if msg[:10] == '+drkv_gen ':
@@ -224,14 +269,16 @@ async def on_message(message):
 
         begin = len(tknew)
 
-        state = model.loadContext([187, 187], tknew, model.empty_state())
+        state = loadContext([187, 187], tknew, emptyState)
 
         with torch.no_grad():
-            state = model.run(
-                currstate=[{"score": 0, "ctx": state[0], "state":state[1]}], endChars=[])
+            for i in range(100):
+                o = model.forward([state[0][-1]], state[1])
+                tok = sample_logits(o[0])
+                state = (state[0] + [tok], o[1])
 
-        send_msg = tokenizer.tokenizer.decode(state[0]["ctx"][begin:]).strip()
-        state = (state[0]["ctx"], state[0]["state"])
+        send_msg = tokenizer.tokenizer.decode(state[0][begin:]).strip()
+        state = state
         print(f'### send ###\n[{send_msg}]')
         await message.reply(send_msg+"\n continue with +drkv_cont "+str(len(storys)))
         storys.append(state)
@@ -244,9 +291,10 @@ async def on_message(message):
         begin = len(state[0])
 
         with torch.no_grad():
-            state = model.run(
-                currstate=[{"score": 0, "ctx": state[0], "state":state[1]}], endChars=[])
-        state = (state[0]["ctx"], state[0]["state"])
+            for i in range(100):
+                o = model.forward([state[0][-1]], state[1])
+                tok = sample_logits(o[0])
+                state = (state[0] + [tok], o[1])
 
         send_msg = tokenizer.tokenizer.decode(state[0][begin:]).strip()
         print(f'### send ###\n[{send_msg}]')
