f94153d34cf2ea63f346fe17fd304afc28deeff1
harrison <harrison.vanderbyl@gmail.com>
2022-12-22 20:45:25 +1100

    streamline

--- src/tensorflowrwkv.py
+++ src/tensorflowrwkv.py
@@ -1,46 +1,117 @@
+from functools import reduce
 import tensorflow as tf
 import torch
 import src.rwkvops
 
+from ast import Delete
+from functools import reduce
+import types
+import torch
+import math
+import os
+import gc
+import torch.nn as nn
+from typing import List, Dict, Tuple, Union
+from torch import autocast
+import numpy as np
+from tqdm import tqdm
 
-def RWKV(mpreprocess, mpostprocess, mlayers, mode="tensorflow", *args, **kwargs):
+
+def RWKV(Path, mode="tensorflow", *args, **kwargs):
+
+    n_layer = 0
+
+    with torch.no_grad():
+        w: Dict[str, torch.Tensor] = torch.load(
+            Path, map_location="cpu")
+        # refine weights and send to correct device
+        keys = list(w.keys())
+        for x in keys:
+            if '.time_' in x:
+                w[x] = w[x].squeeze()
+
+            if '.time_decay' in x:
+                w[x] = w[x].float()
+                w[x] = -torch.exp(w[x])
+
+            if 'receptance.weight' in x:
+                w[x] = -w[x]
+
+            w[x].requires_grad = False
+            w[x] = w[x].to(dtype=torch.bfloat16)
+            try:
+                if (int(x.split('.')[1])+1 > n_layer):
+                    n_layer = int(x.split('.')[1])+1
+            except:
+                pass
+
+    # store weights in self.w
+
+        keys = list(w.keys())
+
+        preprocess = []
+        for x in tqdm(range(len(w["emb.weight"]))):
+            preprocess = preprocess + [torch.layer_norm(w["emb.weight"][x], (w["blocks.0.ln0.weight"].shape[0],),
+                                                        weight=w["blocks.0.ln0.weight"], bias=w["blocks.0.ln0.bias"])]
+
+        for x in range(n_layer):
+            w[f"blocks.{x}.att.key.weight"] *= w[f"blocks.{x}.att.time_mix_k"]
+            w[f"blocks.{x}.att.value.weight"] *= w[f"blocks.{x}.att.time_mix_v"]
+            w[f"blocks.{x}.att.receptance.weight"] *= w[f"blocks.{x}.att.time_mix_r"]
+
+            w[f"blocks.{x}.ffn.key.weight"] *= w[f"blocks.{x}.ffn.time_mix_k"]
+            w[f"blocks.{x}.ffn.receptance.weight"] *= w[f"blocks.{x}.ffn.time_mix_r"]
+
+            w[f"blocks.{x}.att.time_mix_k"] = 1 / \
+                w[f"blocks.{x}.att.time_mix_k"] - 1
+            w[f"blocks.{x}.att.time_mix_v"] = 1 / \
+                w[f"blocks.{x}.att.time_mix_v"] - 1
+            w[f"blocks.{x}.att.time_mix_r"] = 1 / \
+                w[f"blocks.{x}.att.time_mix_r"] - 1
+
+            w[f"blocks.{x}.ffn.time_mix_k"] = 1 / \
+                w[f"blocks.{x}.ffn.time_mix_k"] - 1
+            w[f"blocks.{x}.ffn.time_mix_r"] = 1 / \
+                w[f"blocks.{x}.ffn.time_mix_r"] - 1
+
+    # garbage collect
+
+    gc.collect()
+    torch.cuda.empty_cache()
 
     ops = src.rwkvops.RwkvOpList[mode](
-        len(mlayers), len(mlayers[0]["time_first"]), *args, **kwargs)
+        n_layer, len(w[f"blocks.0.ffn.time_mix_k"]), *args, **kwargs)
 
     class RWKVTFLayer(ops.module):
-        def __init__(self, dic):
+        def __init__(self, x):
             super(RWKVTFLayer, self).__init__()
 
             # self.__dict__ = {k: ops.initTensor(v) for k, v in dic.items()}
 
             # for k, v in dic.items():
             #     print(len(v.shape), v.shape)
-            self.key = ops.initTensor(dic["key"])
-            self.receptance = ops.initTensor(dic["receptance"])
-            self.value = ops.initTensor(dic["value"])
-
-            self.ln1w = ops.initTensor(dic["ln1w"])
-            self.ln1b = ops.initTensor(dic["ln1b"])
-
-            self.ln2w = ops.initTensor(dic["ln2w"])
-            self.ln2b = ops.initTensor(dic["ln2b"])
-
-            self.time_mix_k_ffn = ops.initTensor(dic["time_mix_k_ffn"])
-            self.time_mix_r_ffn = ops.initTensor(dic["time_mix_r_ffn"])
-
-            self.key_ffn = ops.initTensor(dic["key_ffn"])
-            self.receptance_ffn = ops.initTensor(dic["receptance_ffn"])
-            self.value_ffn = ops.initTensor(dic["value_ffn"])
-
-            self.kktk = ops.initTensor(dic["kktk"])
-            self.vvtv = ops.initTensor(dic["vvtv"])
-            self.rrtr = ops.initTensor(dic["rrtr"])
-
-            self.time_first = ops.initTensor(dic["time_first"])
-            self.time_decay = ops.initTensor(dic["time_decay"])
-
-            self.outputvv = ops.initTensor(dic["outputvv"])
+            self.ln1w = ops.initTensor(w[f"blocks.{x}.ln1.weight"])
+            self.ln1b = ops.initTensor(w[f"blocks.{x}.ln1.bias"])
+            self.ln2w = ops.initTensor(w[f"blocks.{x}.ln2.weight"])
+            self.ln2b = ops.initTensor(w[f"blocks.{x}.ln2.bias"])
+            self.time_decay = ops.initTensor(w[f"blocks.{x}.att.time_decay"])
+            self.time_first = ops.initTensor(w[f"blocks.{x}.att.time_first"])
+            self.kktk = ops.initTensor(w[f"blocks.{x}.att.time_mix_k"])
+            self.vvtv = ops.initTensor(w[f"blocks.{x}.att.time_mix_v"])
+            self.rrtr = ops.initTensor(w[f"blocks.{x}.att.time_mix_r"])
+            self.key = ops.initTensor(w[f"blocks.{x}.att.key.weight"])
+            self.value = ops.initTensor(w[f"blocks.{x}.att.value.weight"])
+            self.receptance = ops.initTensor(
+                w[f"blocks.{x}.att.receptance.weight"])
+            self.outputvv = ops.initTensor(w[f"blocks.{x}.att.output.weight"])
+            self.time_mix_k_ffn = ops.initTensor(
+                w[f"blocks.{x}.ffn.time_mix_k"])
+            self.time_mix_r_ffn = ops.initTensor(
+                w[f"blocks.{x}.ffn.time_mix_r"])
+            self.key_ffn = ops.initTensor(w[f"blocks.{x}.ffn.key.weight"])
+            self.receptance_ffn = ops.initTensor(
+                w[f"blocks.{x}.ffn.receptance.weight"])
+            self.value_ffn = ops.initTensor(w[f"blocks.{x}.ffn.value.weight"])
 
         @ ops.layerdef
         def forward(self, x, statea, stateb, statec, stated):
@@ -78,7 +149,7 @@ def RWKV(mpreprocess, mpostprocess, mlayers, mode="tensorflow", *args, **kwargs)
             return x, aaa, bbb, ccc, ddd
 
     class RWKVTFPre(ops.module):
-        def __init__(self, preprocess):
+        def __init__(self):
             super(RWKVTFPre, self).__init__()
             self.preprocess = ops.stack(list(map(ops.initTensor, preprocess)))
 
@@ -87,11 +158,12 @@ def RWKV(mpreprocess, mpostprocess, mlayers, mode="tensorflow", *args, **kwargs)
             return self.preprocess[x[0]]
 
     class RWKVTFPost(ops.module):
-        def __init__(self, postprocess):
+        def __init__(self):
             super(RWKVTFPost, self).__init__()
-            self.postprocess0 = ops.initTensor(postprocess[0])
-            self.postprocess1 = ops.initTensor(postprocess[1])
-            self.postprocess2 = ops.initTensor(postprocess[2])
+
+            self.postprocess0 = ops.initTensor(w["ln_out.weight"])
+            self.postprocess1 = ops.initTensor(w["ln_out.bias"])
+            self.postprocess2 = ops.initTensor(w["head.weight"])
 
         @ ops.postfunc
         def forward(self, x):
@@ -100,14 +172,15 @@ def RWKV(mpreprocess, mpostprocess, mlayers, mode="tensorflow", *args, **kwargs)
 
     class myRWKV(ops.module):
         @ ops.initfunc
-        def __init__(self, preprocess, postprocess, layers):
+        def __init__(self):
             super(myRWKV, self).__init__()
-            self.preprocess = RWKVTFPre(preprocess)
+            self.preprocess = RWKVTFPre()
 
-            self.mylayers: list[RWKVTFLayer] = list(map(
-                RWKVTFLayer, layers))
+            self.mylayers: list[RWKVTFLayer] = []
+            for i in range(n_layer):
+                self.mylayers.append(RWKVTFLayer(i))
 
-            self.postprocess = RWKVTFPost(postprocess)
+            self.postprocess = RWKVTFPost()
 
         @ ops.mainfunc
         def forward(self, x, state):
@@ -132,4 +205,4 @@ def RWKV(mpreprocess, mpostprocess, mlayers, mode="tensorflow", *args, **kwargs)
             # print(len(ot))
             return x, ops.stack(ot)
 
-    return myRWKV(mpreprocess, mpostprocess, mlayers), ops.emptyState
+    return myRWKV(), ops.emptyState
