f0339bd9c931a040209842d397ad70d63277b21b
harrison <harrison.vanderbyl@gmail.com>
2023-01-07 11:25:34 +1100

    load tflite

--- src/rwkv.py
+++ src/rwkv.py
@@ -15,7 +15,8 @@ def RWKV(Path=None, mode=None, *args, **kwargs):
     if (Path == None):
         files = os.listdir()
         # filter by ending in .pth
-        files = [f for f in files if f.endswith(".pth") or f.endswith(".pt")]
+        files = [f for f in files if f.endswith(
+            ".pth") or f.endswith(".pt") or f.endswith(".tflite")]
 
         questions = [
             inquirer.List('file',
@@ -46,23 +47,30 @@ def RWKV(Path=None, mode=None, *args, **kwargs):
 
     elif Path.endswith(".tflite"):
         import tensorflow.lite as tflite
+        import tensorflow as tf
         interpreter = tflite.Interpreter(model_path=Path)
         interpreter.allocate_tensors()
 
         input_details = interpreter.get_input_details()
         output_details = interpreter.get_output_details()
 
+        embed = input_details[1]['shape'][1]
+        layers = input_details[1]['shape'][0]
+        dtype = input_details[1]['dtype']
+
         class InterOp():
             def forward(self, x, y):
 
                 interpreter.set_tensor(
-                    input_details[0]['index'], torch.LongTensor(x).cuda())
-                interpreter.set_tensor(input_details[1]['index'], y.cuda())
+                    input_details[0]['index'], tf.convert_to_tensor(x, dtype=tf.int32))
+                interpreter.set_tensor(
+                    input_details[1]['index'], y)
                 interpreter.invoke()
                 output_data = interpreter.get_tensor(
-                    output_details[0]['index'])
+                    output_details[0]['index']), interpreter.get_tensor(output_details[1]['index'])
+
                 return output_data
-        return InterOp(), torch.tensor([[0.01]*int(embed)]*int(layers)*5)
+        return InterOp(), tf.convert_to_tensor([[0.01]*int(embed)]*int(layers), dtype=dtype)
 
     n_layer = 0
 
