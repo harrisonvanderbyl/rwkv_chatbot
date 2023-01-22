95a2191fc4ead1e5fa566c2d8e076b80bb865040
harrison <harrison.vanderbyl@gmail.com>
2023-01-22 21:36:54 +1100

    change to progressional

--- processPrompts.py
+++ processPrompts.py
@@ -11,15 +11,11 @@ jsonl = [json.loads(line)["question"] for line in jsonl]
 
 
 otputs = []
-
-for x in tqdm.tqdm(jsonl):
-    model.resetState()
-    o, start = model.loadContext("\n",
-                                 f"Prompt: {x}?\n\nLong Detailed Expert Response: ")
-
-    output = model.forward(number=100)["output"]
-    otputs += [{"question": x, "LDER": output}]
-
 with open("output.jsonl", "w") as f:
-    for x in otputs:
-        f.write(json.dumps(x)+"\n")
+    for x in tqdm.tqdm(jsonl):
+        model.resetState()
+        o, start = model.loadContext("\n",
+                                     f"Prompt: {x}?\n\nLong Detailed Expert Response: ")
+
+        output = model.forward(number=100)["output"]
+        f.write(json.dumps({"question": x, "LDER": output})+"\n")
