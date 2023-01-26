6dfcdf28169e90c63a97a0e43bd42470660afbdf
harrison <harrison.vanderbyl@gmail.com>
2023-01-26 22:42:40 +1100

    fix

--- chat.py
+++ chat.py
@@ -24,18 +24,18 @@ async def runDiscordBot(model: RWKVMaster):
         if message.content.startswith("!rwkv "):
             model.resetState()
             model.loadContext(newctx=f"Q: {message.content[6:]}\n\nA: ")
-            text = model.forward(stopStrings="<|endoftext|>", number=100)[
+            text = model.forward(stopStrings=["<|endoftext|>"], number=100)[
                 "output"]
             states[message.id] = model.getState()
-            await message.channel.send(text)
+            await message.reply(text)
 
         # check if message is a reply
         if message.reference is not None:
             if message.reference.message_id in states.keys():
                 model.setState(states[message.reference.message_id])
                 model.loadContext(newctx=f"\n\nQ: {message.content}\n\nA: ")
-                text = model.forward(stopStrings="<|endoftext|>", number=100)[
+                text = model.forward(stopStrings=["<|endoftext|>"], number=100)[
                     "output"]
                 states[message.id] = model.getState()
-                await message.channel.send(text)
+                await message.reply(text)
     await client.start(os.environ.get("TOKEN", input("Discord Token:")))
