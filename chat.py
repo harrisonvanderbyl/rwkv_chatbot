2a000333d1cfc37fc84f7f59cfaf62f8f7e03bcc
harrison <harrison.vanderbyl@gmail.com>
2023-01-27 10:20:08 +1100

    update

--- chat.py
+++ chat.py
@@ -2,8 +2,6 @@
 
 from rwkvstic.rwkvMaster import RWKVMaster
 
-states = {}
-
 
 async def runDiscordBot(model: RWKVMaster):
     import discord
@@ -23,19 +21,10 @@ async def runDiscordBot(model: RWKVMaster):
         # check if message is a command
         if message.content.startswith("!rwkv "):
             model.resetState()
-            model.loadContext(newctx=f"Q: {message.content[6:]}\n\nA: ")
+            model.loadContext(
+                newctx=f"\n\nQuestion: {message.content[6:]}\n\nExpert Long Detailed Response: ")
             text = model.forward(stopStrings=["<|endoftext|>"], number=100)[
                 "output"]
-            states[message.id] = model.getState()
             await message.reply(text)
 
-        # check if message is a reply
-        if message.reference is not None:
-            if message.reference.message_id in states.keys():
-                model.setState(states[message.reference.message_id])
-                model.loadContext(newctx=f"\n\nQ: {message.content}\n\nA: ")
-                text = model.forward(stopStrings=["<|endoftext|>"], number=100)[
-                    "output"]
-                states[message.id] = model.getState()
-                await message.reply(text)
     await client.start(os.environ.get("TOKEN", input("Discord Token:")))
