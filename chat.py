b3d681ded0f1bfabfde4dff02e4b187c114c3f6b
harrison <harrison.vanderbyl@gmail.com>
2023-01-30 23:59:57 +1100

    add some codegen to chat

--- chat.py
+++ chat.py
@@ -40,4 +40,31 @@ async def runDiscordBot(model: RWKVMaster):
             await asyncio.sleep(1)
             await mess.edit(content=tex)
 
+        if message.content.startswith("!code"):
+            text = message.content[6:]
+            language = text.split(" ")[0]
+            comment = text.split(" ")[1:]
+
+            await message.channel.send("Loading...")
+
+            model.resetState()
+            model.loadContext(
+                newctx=f"Q: Please write a {language} program that {comment}\n\nA:")
+            model.loadContext(
+                newctx=f"```Sure, here is a {language} program that {comment}:<code>//{comment}\n")
+            tex = ""
+            for i in range(20):
+                print(i)
+                curr = model.forward(number=10, stopStrings=["</code>"])[
+                    "output"]
+                tex = tex + curr
+                print(curr)
+
+                if ("<|endoftext|>" in curr or "</code>" in curr):
+                    break
+                mess = await mess.edit(content=tex)
+
+            await asyncio.sleep(1)
+            await mess.edit(content=tex)
+
     await client.start(os.environ.get("TOKEN", input("Discord Token:")))
