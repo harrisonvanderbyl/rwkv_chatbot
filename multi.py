44a99c55f90cd9ed34cf34f6f250b2f6a5b30520
harrison <harrison.vanderbyl@gmail.com>
2023-02-06 20:26:21 +1100

    better

--- multi.py
+++ multi.py
@@ -4,13 +4,19 @@ import inquirer
 from chat import runDiscordBot
 from rwkvstic.load import RWKV
 
-services = inquirer.checkbox(
-    message="What services do you want to use?",
-    choices=[
-        "Web Interface",
-        "discord bot",
-    ]
-)
+services = []
+
+while len(services) == 0:
+
+    services = inquirer.checkbox(
+        message="What services do you want to use?(MULTI CHOICE, use space to select options)",
+        choices=[
+            "Web Interface",
+            "discord bot",
+        ]
+    )
+    if len(services) == 0:
+        print("Please select at least one service!")
 model = RWKV()
 
 # run async simultaneously
